2f0664fa66fe9cb0b637a6d7bff8210ab8708cce
Aleksei Kowalski <aleksey.kowalskiy@gmail.com>
2020-09-02 13:04:43 +0300

    Revert "create rep"
    This reverts commit e1c84cea0bfadedbb0d632522d5b589de02e2d1f.

--- backend/services/reg.service.js
+++
@@ -1,8 +0,0 @@
-const DbService = require("moleculer-db");
-const SqlAdapter = require("moleculer-db-adapter-sequelize");
-const Sequelize = require("sequelize");
-
-module.exports = {
-  name: "registration",
-  mixins: [DbService],
-};
